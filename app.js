ed3b6cdf899c0bb4e8e259aa0b0e2c498212abbe
jasonbcox <jasonbcox@gmail.com>
2018-01-11 11:13:16 -0700

    added search form

--- app.js
+++ app.js
@@ -37,6 +37,96 @@ app.use(routes);
 
 //adding html endpoints for API
 
+app.get('/', function homepage(req, res) {
+    res.sendFile(__dirname + '/views/index.ejs');//do i need a splash page + a homepage?
+
+  });
+
+  /**********
+ * ROUTES *
+ **********/
+
+// Serve static files from the `/public` directory:
+// i.e. `/images`, `/scripts`, `/styles`
+app.use(express.static('public'));
+
+
+/************
+ * DATABASE *
+ ************/
+
+// var db = require('./models');
+
+
+//json endpoints  
+
+app.get('/api', function api_index(req, res) {
+    // what do I need to keep out of here
+    res.json({
+      woops_i_has_forgot_to_document_all_my_endpoints: false, // CHANGE ME ;)
+      message: "Here's the dirt",
+      documentation_url: "", // CHANGE ME
+      base_url: "https://peaceful-escarpment-87562.herokuapp.com/", // this is c
+      endpoints: [
+        {method: "GET", path: "/api", description: "Describes all available endpoints"},
+        {method: "GET", path: "/api/profile", description: "Data about me"},
+        {method: "GET", path: "/api/sandwiches", description: "Get ALL the sammiches"},
+        {method: "POST", path: "/api/sandwiches", description: "Create a new sammich"},
+        {method: "PUT", path: "/api/sandwiches", description: "Update a sammich"},
+    //     {method: "DELETE", path: "/api/sandwiches", description: "Delete a sammich :("}
+       ]
+    })
+  });
+ 
+  
+  //get all movies, does not make sense, needs to be search 
+  app.get('/api/movie', function (req, res){
+    db.Sandwich.find(function(err, sandwich){
+      res.json(sandwich);
+      });
+    });
+  
+    //get one movie by id
+    app.get('/api/movie/:id', function (req, res){
+      db.Sandwich.findById(req.params.id, function(err, sandwich){
+    if (err) {return console.log("Cannot find this movie", + err)}
+        res.json(sandwich);
+      });      
+    });
+    
+  
+  // post route needs to create new user
+//   app.post('/api/sandwich', function (req, res){
+//     var newSandwich = new db.Sandwich({
+//       description: req.body.description,
+//       bread: req.body.bread,
+//       protein: req.body.protein,
+//       condiment: req.body.condiment,
+//       length: req.body.length
+//     });
+//     newSandwich.save(function (err, sandwich){
+//       if (err) {
+//         return console.log("save error:" + err);
+//       }
+//       console.log('saved');
+//       res.json(sandwich);
+//     });
+//   });
+  //update user somehow
+  app.put('/api/user/:id', function (req, res){
+    db.Sandwich.findOneAndUpdate({_id: req.params.id}, {$set: {description: req.body.description, bread: req.body.bread, protein: req.body.protein, condiment: req.body.condiment, length: req.body.length}}, {new: true}, function(err, sandwich){
+  if (err) {return console.log("you suck", + err)}
+      res.json(sandwich);
+    });      
+  });
+  
+  //delete route needs to delete user account or favorite
+//   app.delete('/api/sandwich/:id', function (req, res){
+//     db.Sandwich.remove({_id: req.params.id}, function (err, sandwich) {
+//   if (err) {return console.log("you suck", + err)}
+//       res.json(sandwich);
+//     });      
+//   });
 
 
 app.listen(process.env.PORT || 3000, function () {
